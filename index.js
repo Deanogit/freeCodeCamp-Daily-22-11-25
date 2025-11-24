9d30823cd785ce1c0c9a99813ffcd524b4f1920e
Dean Howe <hideanhowe@gmail.com>
2025-11-24 19:03:35 +0000

    shift unshift

--- index.js
+++ index.js
@@ -5,18 +5,40 @@
 // Scale the quantity by the given number. Do not include any trailing zeros and do not convert any units.
 // Return the scaled items in the same order they are given.
 
+// function scaleRecipe(ingredients, scale) {
+// console.log("This is whats in the arguments:", ingredients, scale)
+// returns ["2 C Flour", "1.5 T Sugar"], 2
+// loop through ingredients and return a new array
+// const array = ingredients.map((x) => x.split(" "))
+// console.log("This is whats in the ingredients array", array[1])
+// multiply the first array item by scale
+// const newArray = array.map((x) => `${x[0] * scale} ${x.map((y) => y.join(" "))}`);
+// console.log("This is what is in the new array", newArray)
+// I'm missing some of the arrays ...
+// return ingredients;
+//  return newArray
+//}
+
 function scaleRecipe(ingredients, scale) {
-  console.log('This is whats in the arguments:', ingredients, scale);
-  // returns ["2 C Flour", "1.5 T Sugar"], 2
-  // loop through ingredients and return a new array
-  const array = ingredients.map((x) => x.split(' '));
-  console.log('This is whats in the ingredients array', array[1]);
-  // multiply the first array item by scale
-  const newArray = array.map(
-    (x) => `${x[0] * scale} ${x.map((y) => y.join(' '))}`
-  );
-  console.log('This is what is in the new array', newArray);
-  // I'm missing some of the arrays ...
-  // return ingredients;
-  return newArray;
+  // for each ingredient
+  // split up ingredients into an array
+  const ingredientsArray = ingredients.map((x) => x.split(' '));
+  console.log(ingredientsArray);
+  // take the first entry
+  //const newEntry = ingredientsArray.map((x) => x[0] * scale)
+  //ingredientsArray[0] = ingredientsArray[0] * scale;
+  // console.log(newEntry)
+  // remove it
+  // replace it with first entry * scale
+  // join ingredients
+  // return ingredients
 }
+
+// The shift() method removes the first item of an array.
+
+// The shift() method changes the original array.
+
+// The shift() method returns the shifted element.
+
+// The unshift() method adds new elements to the beginning of an array.
+// The unshift() method overwrites the original array.
